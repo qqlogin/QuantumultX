a0f500a45be403e037a77618a61f842b4bfe33ac
qqlogin <69336398+qqlogin@users.noreply.github.com>
2021-03-02 09:18:47 +0800

    Update IP_bili_cn.js

--- IP_bili_cn.js
+++ IP_bili_cn.js
@@ -54,5 +54,5 @@ emoji=City_ValidCheck(obj['province']) == "澳门"? "️🇲🇴️":emoji
 var title =  emoji +'『'+ City_ValidCheck(obj['province'])+'』';//+Area_check(obj['country']);
 var subtitle =  ISP_ValidCheck(obj['isp']) + " ➠ "+ obj['country'];
 var ip = obj['addr']; 
-var description = '服务商:'+obj['isp'] + '\n'+'定位: [' +obj["latitude"]+","+obj["longitude"]+"]"+ '\n' + 'IP:'+ obj['addr'] + '\n' +'时区:'+ obj['timezone'];
+var description = '服务商:'+obj['isp'] + '\n'+'定位: [' +obj["latitude"]+","+obj["longitude"]+"]"+ '\n' + 'IP:'+ obj['addr'];
 $done({title, subtitle, ip, description});
